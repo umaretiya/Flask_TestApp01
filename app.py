f848390256cfdd5851107587e00ce8a2c40c5448
umaretiya <umaretiya@gmail.com>
2022-07-02 16:39:53 +0530

    first

---
+++ app.py
@@ -0,0 +1,23 @@
+from cv2 import RQDecomp3x3
+from flask import Flask,render_template,request
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST','GET'])
+def home():
+    return render_template("index.html")
+
+@app.route('/check', methods=['POST','GET'])
+def result():
+    if request.method == 'POST':
+        name = request.form['name']
+        surname = request.form['surname']
+        number = request.form['number']
+        data = {'name':name,"surname":surname,'number':number}
+        return render_template('home.html', context=data)
+    return render_template("index.html")   
+        
+    
+if __name__ == '__main__':
+    app.run(debug=True)
